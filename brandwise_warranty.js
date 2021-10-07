5a08737e6cc7b5ee4237482ec65bd6ff1ee6912a
rajkris-cipherco <92093362+rajkris-cipherco@users.noreply.github.com>
2021-10-07 21:42:38 +0530

    Update brandwise_warranty.js

--- brandwise_warranty.js
+++ brandwise_warranty.js
@@ -1,5 +1,5 @@
 (function() {
-  document.getElementById("brandwise-btn").innerHTML = '<button type="button" onclick=alert(\"Hello from Brandwise!!!!!\")>Click Me!</button>';
+  document.getElementById("brandwise-btn").innerHTML = "<button type='button' onclick=alert('Hello from Brandwise!!!!!')>Click Me!</button>";
 
 })();
 
